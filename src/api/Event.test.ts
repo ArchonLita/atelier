1e67ea9a839af890ad5292a0e5eec9965e6bfe67
ArchonLita <archonlita@gmail.com>
2023-09-22 15:08:40 -0400

    updated event handler tests

--- src/api/Event.test.ts
+++ src/api/Event.test.ts
@@ -1,14 +1,41 @@
-import { test, mock, expect } from "bun:test";
+import { expect, mock, test } from "bun:test";
 import { Emitter } from "./Event";
 
-test("Listener called after event emitted", () => {
-  const emitter = new Emitter();
+type TestEvents = {
+  eventA: number;
+  eventB: string;
+  eventC: undefined;
+};
+
+class TestEmitter extends Emitter<TestEvents> { }
+
+test("listener called after event emitted", () => {
+  const emitter = new TestEmitter();
+  const mockHandlerA = mock(() => undefined);
+  const mockHandlerB = mock(() => undefined);
+  const mockHandlerC = mock(() => undefined);
+
+  emitter.addListener("eventA", mockHandlerA);
+  emitter.addListener("eventB", mockHandlerB);
+  emitter.addListener("eventC", mockHandlerC);
+
+  emitter.call("eventA", 39);
+  emitter.call("eventB", "test data");
+  emitter.call("eventC", undefined);
+
+  expect(mockHandlerA.mock.calls).toEqual([[39]]);
+  expect(mockHandlerB.mock.calls).toEqual([["test data"]]);
+  expect(mockHandlerC.mock.calls).toEqual([[undefined]]);
+});
+
+test("listener is not called after being removed", () => {
+  const emitter = new TestEmitter();
   const mockHandler = mock(() => undefined);
 
-  emitter.addListener("test", {
-    on: mockHandler,
-  });
-  emitter.call("test", "testData");
+  emitter.addListener("eventB", mockHandler);
+  emitter.call("eventB", "test data 1");
+  emitter.removeListener("eventB", mockHandler);
+  emitter.call("eventB", "test data 2");
 
-  expect(mockHandler.mock.calls).toEqual([["test", "testData"]]);
+  expect(mockHandler.mock.calls).toEqual([["test data 1"]]);
 });
--- src/api/Event.ts
+++ src/api/Event.ts
@@ -1,16 +1,30 @@
-export class Emitter {
-  listeners = new Map<string, Set<Listener>>();
-
-  addListener(id: string, listener: Listener) {
-    if (!this.listeners.has(id)) this.listeners.set(id, new Set());
-    this.listeners.get(id)!.add(listener);
-  }
-
-  call(event: string, data?: any) {
-    this.listeners.get(event)?.forEach((listener) => listener.on(event, data));
-  }
+interface EventMap {
+  [key: string]: any;
 }
 
-export interface Listener {
-  on: (event: string, data?: any) => void;
+export type Listener<T extends EventMap> = (
+  data: T[Extract<keyof T, string>],
+) => void;
+
+export class Emitter<T extends EventMap> {
+  listeners = new Map<Extract<keyof T, string>, Set<Listener<T>>>();
+
+  addListener<E extends Extract<keyof T, string>>(
+    event: E,
+    listener: Listener<T>,
+  ) {
+    if (!this.listeners.has(event)) this.listeners.set(event, new Set());
+    this.listeners.get(event)!.add(listener);
+  }
+
+  removeListener<E extends Extract<keyof T, string>>(
+    event: E,
+    listener: Listener<T>,
+  ): boolean {
+    return !!this.listeners.get(event)?.delete(listener);
+  }
+
+  call<E extends Extract<keyof T, string>>(event: E, data: T[E]) {
+    this.listeners.get(event)?.forEach((listener) => listener(data));
+  }
 }
