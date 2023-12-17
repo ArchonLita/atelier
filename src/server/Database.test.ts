cb8752b48d0530519747f13ef5d444fc6aee5e0b
ArchonLita <archonlita@gmail.com>
2023-12-16 19:17:43 -0500

    Create file database for character sheets

---
+++ src/server/Database.test.ts
@@ -0,0 +1,32 @@
+import { expect, test } from "bun:test";
+import { Sheet } from "../dnd/Sheet";
+import { Database } from "./Database";
+import { Glob } from "bun";
+
+const baseAbilityScores = {
+  strength: 8,
+  wisdom: 10,
+  charisma: 12,
+  dexterity: 13,
+  constitution: 14,
+  intelligence: 15,
+};
+
+const id = "TEST-SHEET";
+const db = new Database("./test");
+
+test("Writes sheets to file database", async () => {
+  const sheet = new Sheet();
+  sheet.baseAbilityScores = baseAbilityScores;
+  sheet.load();
+
+  db.sheets.set(id, sheet);
+
+  await db.save();
+  await db.load();
+
+  expect(db.sheets.get(id)).toEqual(sheet);
+
+  const files = await Array.fromAsync(new Glob("*.json").scan("./test/sheets"));
+  expect(files).toEqual(["TEST-SHEET.json"]);
+});
